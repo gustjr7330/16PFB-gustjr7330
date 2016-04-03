74617a0c117a7c45007003243c63900aba82ecc5
조현석 <gustjr7330@gmail.com>
2016-04-03 20:42:25 +0900

    commit 변경...

---
+++ ex06.py
@@ -0,0 +1,20 @@
+x = "There are %d types of people." % 10 # balbalbalbal
+binary = "binary" # asdsadsadas
+do_not = "don't" # asdasdasdas
+y = "Those who know %s and those who %s." % (binary, do_not) # sadgd
+
+print x # sadasdasda
+print y # sakldad
+
+print "I said: %r." % x # sadsa
+print "I also said: '%s'." % y # asdsasa
+
+hilarious = False
+joke_evaluation = "Isn't that joke so funny?! %r"
+
+print joke_evaluation % hilarious
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+print w + e
---
+++ ex11.py
@@ -0,0 +1,9 @@
+print "How old are you?",
+age = raw_input(20)
+print "How tall are you?",
+height = raw_input(200)
+print "How much do you weigh?",
+weight = raw_input(50)
+
+print "So, you're %r old, %r tall and %r heavy." % (
+    age, height, weight)
