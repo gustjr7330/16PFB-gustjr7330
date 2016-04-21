ae81d41f824b066f469f56bbae170feaf6266864
JHS <gustjr7330@gmail.com>
2016-04-21 23:36:25 +0900

    commit 내용 입력

---
+++ ex-z.py
@@ -0,0 +1,22 @@
+def ipad_and_iphone(ipad_count, iphone_count):
+    print "I have a %d ipad." % (ipad_count)
+    print "I have a %d iphone." % (iphone_count)
+
+
+print "I have a %d ipad." % (5)
+print "I have a %d iphone" % (6)
+
+
+ipad_and_iphone(20, 30)
+
+amount_of_ipad = 20
+amount_of_iphone = 50
+
+ipad_and_iphone(amount_of_ipad, amount_of_iphone)
+
+ipad_and_iphone(amount_of_ipad + 20, amount_of_iphone + 30)
+
+
+
+
+
